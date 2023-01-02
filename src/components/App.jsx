a996ab9b682c766f8da95dc80316b2a8b006eab7
Josh Taylor <josht@duck.com>
2023-01-02 18:18:45 +1000

    add calculator layout and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,32 @@
-import '../styles/App.css'
+import '../styles/App.css';
 
 function App() {
-
   return (
-    <div className="App">
-
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      <button className="span-two">C</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>×</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>-</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>+</button>
+      <button>0</button>
+      <button>.</button>
+      <button className="span-two">=</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
